7244233dda5206a267b449cb81d539770f43ee62
yousufbilal <64967817+yousufbilal@users.noreply.github.com>
2025-02-20 20:00:19 +0000

    removed index.js

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
-import { MongoClient } from 'mongodb';
+// import { MongoClient } from 'mongodb';
 
-const client = new MongoClient("mongodb+srv://userDB:passwordDB@cluster0.7lpxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+// const client = new MongoClient("mongodb+srv://userDB:passwordDB@cluster0.7lpxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
-async function sendData() {
-    let MonsterMovies = [{
-        movie1: "Nosfaratu",
-        movie2: "Dracula",
-        movie3: "Wolfman",
-    }]
+// async function sendData() {
+//     let MonsterMovies = [{
+//         movie1: "Nosfaratu",
+//         movie2: "Dracula",
+//         movie3: "Wolfman",
+//     }]
 
-    let MonsterMovies2 = [{
-        movie1: "shin godzilla",
-        movie2: "godzilla vs kong",
-        movie3: "godzilla 0", 
-    }]
-    try {
-        // Connect to MongoDB
-        await client.connect();
+//     let MonsterMovies2 = [{
+//         movie1: "shin godzilla",
+//         movie2: "godzilla vs kong",
+//         movie3: "godzilla 0", 
+//     }]
+//     try {
+//         // Connect to MongoDB
+//         await client.connect();
 
-        //********SENDING DATA********/
-        // const insertSingleData = await client.db("data").collection("changestream").insertOne({ movie: "yousuf" })
-        // const insertMultipleData = await client.db("data").collection("changestream").insertMany(MonsterMovies)
+//         //********SENDING DATA********/
+//         // const insertSingleData = await client.db("data").collection("changestream").insertOne({ movie: "yousuf" })
+//         // const insertMultipleData = await client.db("data").collection("changestream").insertMany(MonsterMovies)
 
-        //********RETRIEVE DATA********/
-        // const retrieveSingleData = await client.db("data").collection("changestream").findOne({ movie: "Nossssfaratu" })
-        // const retrieveALLData = await client.db("data").collection("changestream").find({}).toArray()
+//         //********RETRIEVE DATA********/
+//         // const retrieveSingleData = await client.db("data").collection("changestream").findOne({ movie: "Nossssfaratu" })
+//         // const retrieveALLData = await client.db("data").collection("changestream").find({}).toArray()
 
-        //********UPDATE DATA********/
-        // const updateSingleData = await client.db("data").collection("changestream").updateOne({ name: "yousuf" }, { $set: { name: "goku" } })
-        // const updateALLData = await client.db("data").collection("changestream").updateMany({ movie: "yousuf" }, { $set: { movie: "dragon ball z" } });
-        // const replaceOneData = await client.db("data").collection("changestream").replaceOne({ movie: "yousuf" }, { movie: "nosfaratu", genre: "gothic horror", year: 2025 })
+//         //********UPDATE DATA********/
+//         // const updateSingleData = await client.db("data").collection("changestream").updateOne({ name: "yousuf" }, { $set: { name: "goku" } })
+//         // const updateALLData = await client.db("data").collection("changestream").updateMany({ movie: "yousuf" }, { $set: { movie: "dragon ball z" } });
+//         // const replaceOneData = await client.db("data").collection("changestream").replaceOne({ movie: "yousuf" }, { movie: "nosfaratu", genre: "gothic horror", year: 2025 })
 
-        //********DELETE DATA********/
-        // const deleteSingleData = await client.db("data").collection("changestream").deleteOne({ movie: "dragon ball z" })
-        // const deleteAllData = await client.db("data").collection("changestream").deleteMany({ movie: "yousuf" })
-        // const dropCollectionData = await client.db("data").collection("changestream").drop()
+//         //********DELETE DATA********/
+//         // const deleteSingleData = await client.db("data").collection("changestream").deleteOne({ movie: "dragon ball z" })
+//         // const deleteAllData = await client.db("data").collection("changestream").deleteMany({ movie: "yousuf" })
+//         // const dropCollectionData = await client.db("data").collection("changestream").drop()
 
-        console.log(updateALLData)
+//         // console.log(updateALLData)
 
-    } catch (error) {
-        console.error('Error:', error);
-    } finally {
-        // Close the connection
-        await client.close();
-    }
-}
+//     } catch (error) {
+//         console.error('Error:', error);
+//     } finally {
+//         // Close the connection
+//         await client.close();
+//     }
+// }
 
-sendData();
+// sendData();
 
 
-// "scripts": {
-//     "start": "node server.js",
-//     "dev": "nodemon server.js"
-//   }
+// // "scripts": {
+// //     "start": "node server.js",
+// //     "dev": "nodemon server.js"
+// //   }
