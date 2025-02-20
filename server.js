0251e5a64e898c44b1585c8a1265d91d840c3f56
yousufbilal <64967817+yousufbilal@users.noreply.github.com>
2025-02-20 20:04:42 +0000

    added the render required syntax

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ async function connectDB() {
 
 app.use('/api', UserRoutes);
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;  // Use Render's port or default to 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
     connectDB();
 });
